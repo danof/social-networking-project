850ba0044c422e72984b6d6f8af527f7616bbf44
Daniel Horlitz <dhorlitz@gmail.com>
2014-11-13 16:36:27 -0500

    Chapter 4 - server, db

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var Post = require('./models/post')
+
+var app = express()
+app.use(bodyParser.json())
+
+app.get('/api/posts', function (req, res, next) {
+  Post.find(function(err, posts) {
+    if (err) { return next(err) }
+    res.json(posts)
+  })
+})
+
+app.post('/api/posts', function (req, res, next) {
+  var post = new Post({
+    username: req.body.username,
+    body: req.body.body
+  })
+  post.save(function (err, post) {
+    if (err) { return next(err) }
+    res.json(201, post)
+  })
+})
+
+app.listen(3000, function() {
+  console.log('Server listening on', 3000)
+})
