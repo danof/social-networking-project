c695874bf3ead528c993abc357a41f5717d5f13c
Daniel Horlitz <dhorlitz@gmail.com>
2014-11-18 16:53:37 -0500

    Chapter 6 - add gulp, automate angular build

---
+++ ng/posts.ctrl.js
@@ -0,0 +1,18 @@
+angular.module('app')
+.controller('PostsCtrl', function ($scope, PostsSvc) {
+  $scope.addPost = function () {
+    if ($scope.postBody) {
+      PostsSvc.create({
+        username: 'dickeyxxx',
+        body: $scope.postBody
+      }).success(function (post) {
+        $scope.posts.unshift(post)
+        $scope.postBody = null
+      })
+    }
+  }
+
+  PostsSvc.fetch().success(function (posts) {
+    $scope.posts = posts
+  })
+})
