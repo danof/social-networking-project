aafba25b8d9ad07f8902d6bcf29832cb458c5c34
Daniel Horlitz <dhorlitz@gmail.com>
2014-12-11 14:51:46 -0500

    Chapter 8 - added logged in user to nav, username lookup for posts

--- ng/login.ctrl.js
+++ ng/login.ctrl.js
@@ -2,8 +2,8 @@ angular.module('app')
 .controller('LoginCtrl', function ($scope, UserSvc) {
   $scope.login = function (username, password) {
     UserSvc.login(username, password)
-    .then(function (user) {
-      console.log(user)
+    .then(function (response) {
+      $scope.$emit('login', response.data)
     })
   }
 })
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser')
 var app = express()
 app.use(bodyParser.json())
 
+app.use(require('./auth'))
 app.use('/api/posts', require('./controllers/api/posts'))
 app.use('/api/sessions', require('./controllers/api/sessions'))
 app.use('/api/users', require('./controllers/api/users'))
