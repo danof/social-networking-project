e4c7f98e4f3ac576e866b9e4aa42f613f1d83373
Daniel Horlitz <dhorlitz@gmail.com>
2014-12-23 16:43:59 -0500

    Chapter 9 - add websockets to posts controller

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use('/api/sessions', require('./controllers/api/sessions'))
 app.use('/api/users', require('./controllers/api/users'))
 app.use(require('./controllers/static'))
 
-app.listen(3000, function() {
+var server = app.listen(3000, function() {
   console.log('Server listening on', 3000)
 })
+require('./websockets').connect(server)
